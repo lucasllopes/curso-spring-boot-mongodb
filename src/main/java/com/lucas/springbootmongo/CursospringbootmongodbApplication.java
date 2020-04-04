4e87a4f96ceb291144038cbc0fee51fe475989af
Lucas Lopes <lucas.llopes99@gmail.com>
2020-04-04 16:12:37 -0300

    Primeiro commit. Projeto iniciado.

---
+++ src/main/java/com/lucas/springbootmongo/CursospringbootmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.springbootmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringbootmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringbootmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/springbootmongo/CursospringbootmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.springbootmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringbootmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
